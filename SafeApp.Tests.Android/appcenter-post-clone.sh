19f0b2bca4d07285eb6cf62dec37e9d1554157b6
Viv-Rajkumar <viv.rajkumar@maidsafe.net>
2017-11-17 14:08:02 +0000

    ci: move post clone scripts to test project folders

--- SafeApp.Tests.Android/appcenter-post-clone.sh
+++ SafeApp.Tests.Android/appcenter-post-clone.sh
@@ -4,6 +4,8 @@ archive_name=alpha-2.zip
 android_dst=SafeApp.AppBindings.Android/lib
 ios_dst=SafeApp.AppBindings.iOS/lib
 
+cd ..
+
 curl -o $archive_name "https://s3.eu-west-2.amazonaws.com/safe-app-csharp/$archive_name"
 
 rm -rf $android_dst
