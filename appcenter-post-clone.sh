7ab2b7e664f30f68a556ef6d19448d00d1ac4904
Viv-Rajkumar <viv.rajkumar@maidsafe.net>
2017-11-17 13:29:41 +0000

    ci: add post clone script to pull native libs

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+archive_name=alpha-2.zip
+android_dst=SafeApp.AppBindings.Android/lib
+ios_dst=SafeApp.AppBindings.iOS/lib
+
+curl -o $archive_name "https://s3.eu-west-2.amazonaws.com/safe-app-csharp/$archive_name"
+
+rm -rf $android_dst
+rm -rf $ios_dst
+
+unzip -q $archive_name
+
+mv android/lib $android_dst
+mv ios/lib $ios_dst
+
+rm -rf android
+rm -rf ios
