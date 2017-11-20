f51b42714368a97de15322193edfb5bb015ee4f2
rachit3107 <32024949+rachit3107@users.noreply.github.com>
2017-11-20 12:49:54 +0530

    feat/[Cipher-Opt]:
    Symmetric and Asymmetric Functions are added to the Cipher-Opt file along with the test cases.

--- SafeApp.Utilities/NativeDelegates.cs
+++ SafeApp.Utilities/NativeDelegates.cs
@@ -18,8 +18,6 @@ namespace SafeApp.Utilities {
   public delegate void EncodeAuthReqCb(IntPtr self, FfiResult result, uint requestId, string encodedReq);
 
   public delegate void EncGenerateKeyPairCb(IntPtr self, FfiResult result, ulong encPubKeyHandle, ulong encSecKeyHandle);
-
-  public delegate void SignGenerateKeyPairCb(IntPtr self, FfiResult result, ulong signPubKeyHandle, ulong signSecKeyHandle);
   public delegate void MDataEntriesForEachCb(
     IntPtr self,
     IntPtr entryKey,
