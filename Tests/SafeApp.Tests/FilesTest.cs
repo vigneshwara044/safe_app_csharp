74a1b4de4f16fa8d8ef61b34212bba1325cef9b2
oetyng <oetyng@gmail.com>
2019-10-04 23:14:39 +0200

    refactor/tests: add teardown with test data cleanup REFERENCE: #106

--- Tests/SafeApp.Tests/FilesTest.cs
+++ Tests/SafeApp.Tests/FilesTest.cs
@@ -8,21 +8,17 @@ namespace SafeApp.Tests
     [TestFixture]
     internal class FilesTest
     {
-        private string _dirName = TestUtils.GetRandomString(5);
+        [OneTimeSetUp]
+        public void Setup() => TestUtils.PrepareTestData();
 
-        [SetUp]
-        public void PrepareTestData()
-        {
-            Directory.CreateDirectory(_dirName);
-            var testFilePath = Path.Combine(_dirName, "index.html");
-            File.WriteAllText(testFilePath, TestUtils.GetRandomString(20));
-        }
+        [OneTimeTearDown]
+        public void TearDown() => TestUtils.RemoveTestData();
 
         [Test]
         public async Task FilesContainerCreateAndGetTest()
         {
             var session = await TestUtils.CreateTestApp();
-            var (xorUrl, processedFiles, filesMap1) = await session.Files.FilesContainerCreateAsync(_dirName, null, true, false);
+            var (xorUrl, processedFiles, filesMap1) = await session.Files.FilesContainerCreateAsync(TestUtils.TestDataDir, null, true, false);
             Assert.NotNull(processedFiles.Files.Find(q => q.FileName.Equals("index.html")));
             Assert.NotNull(xorUrl);
             Assert.NotNull(processedFiles);
@@ -36,11 +32,11 @@ namespace SafeApp.Tests
         public async Task FilesContainerSyncTest()
         {
             var session = await TestUtils.CreateTestApp();
-            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(_dirName, null, true, false);
-            Directory.CreateDirectory($"{_dirName}/newDir");
-            var testFilePath = Path.Combine($"{_dirName}/newDir", "hello.html");
+            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(TestUtils.TestDataDir, null, true, false);
+            Directory.CreateDirectory($"{TestUtils.TestDataDir}/newDir");
+            var testFilePath = Path.Combine($"{TestUtils.TestDataDir}/newDir", "hello.html");
             File.WriteAllText(testFilePath, TestUtils.GetRandomString(20));
-            var (version, newProcessedFiles, newFileMap) = await session.Files.FilesContainerSyncAsync($"{_dirName}/newDir", xorUrl, true, false, false, false);
+            var (version, newProcessedFiles, newFileMap) = await session.Files.FilesContainerSyncAsync($"{TestUtils.TestDataDir}/newDir", xorUrl, true, false, false, false);
             Assert.NotNull(newProcessedFiles.Files.Find(q => q.FileName.Equals("hello.html")));
 
             Assert.AreEqual(1, version);
@@ -52,11 +48,11 @@ namespace SafeApp.Tests
         public async Task FilesContainerSyncDryRunTest()
         {
             var session = await TestUtils.CreateTestApp();
-            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(_dirName, null, true, false);
-            Directory.CreateDirectory($"{_dirName}/newDir");
-            var testFilePath = Path.Combine($"{_dirName}/newDir", "hello.html");
+            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(TestUtils.TestDataDir, null, true, false);
+            Directory.CreateDirectory($"{TestUtils.TestDataDir}/newDir");
+            var testFilePath = Path.Combine($"{TestUtils.TestDataDir}/newDir", "hello.html");
             File.WriteAllText(testFilePath, TestUtils.GetRandomString(20));
-            var (version, newProcessedFiles, newFileMap) = await session.Files.FilesContainerSyncAsync($"{_dirName}/newDir", xorUrl, true, false, false, true);
+            var (version, newProcessedFiles, newFileMap) = await session.Files.FilesContainerSyncAsync($"{TestUtils.TestDataDir}/newDir", xorUrl, true, false, false, true);
             Assert.NotNull(newProcessedFiles.Files.Find(q => q.FileName.Equals("hello.html")));
 
             Assert.AreEqual(1, version);
@@ -68,11 +64,11 @@ namespace SafeApp.Tests
         public async Task FilesContainerAddTest()
         {
             var session = await TestUtils.CreateTestApp();
-            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(_dirName, null, false, false);
-            var testFilePath = Path.Combine(_dirName, "hello.html");
+            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(TestUtils.TestDataDir, null, false, false);
+            var testFilePath = Path.Combine(TestUtils.TestDataDir, "hello.html");
             File.WriteAllText(testFilePath, TestUtils.GetRandomString(20));
             var newFileName = $"{xorUrl}/hello.html";
-            var (version, newProcessedFiles, newfilesMap) = await session.Files.FilesContainerAddAsync($"{_dirName}/hello.html", newFileName, false, false, false);
+            var (version, newProcessedFiles, newfilesMap) = await session.Files.FilesContainerAddAsync($"{TestUtils.TestDataDir}/hello.html", newFileName, false, false, false);
             Assert.AreEqual(1, version);
             Assert.NotNull(newProcessedFiles.Files.Find(q => q.FileName.Equals("hello.html")));
             Assert.AreNotEqual(processedFiles, newProcessedFiles);
@@ -82,11 +78,11 @@ namespace SafeApp.Tests
         public async Task FilesContainerAddDryRunTest()
         {
             var session = await TestUtils.CreateTestApp();
-            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(_dirName, null, false, true);
-            var testFilePath = Path.Combine(_dirName, "hello.html");
+            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(TestUtils.TestDataDir, null, false, true);
+            var testFilePath = Path.Combine(TestUtils.TestDataDir, "hello.html");
             File.WriteAllText(testFilePath, TestUtils.GetRandomString(20));
             var newFileName = $"{xorUrl}/hello.html";
-            var (version, newProcessedFiles, newfilesMap) = await session.Files.FilesContainerAddAsync($"{_dirName}/hello.html", newFileName, false, false, true);
+            var (version, newProcessedFiles, newfilesMap) = await session.Files.FilesContainerAddAsync($"{TestUtils.TestDataDir}/hello.html", newFileName, false, false, true);
             Assert.AreEqual(1, version);
             Assert.NotNull(newProcessedFiles.Files.Find(q => q.FileName.Equals("hello.html")));
             Assert.AreNotEqual(processedFiles, newProcessedFiles);
@@ -97,7 +93,7 @@ namespace SafeApp.Tests
         public async Task FilesContainerAddFromRawTest()
         {
             var session = await TestUtils.CreateTestApp();
-            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(_dirName, null, false, false);
+            var (xorUrl, processedFiles, filesMap) = await session.Files.FilesContainerCreateAsync(TestUtils.TestDataDir, null, false, false);
             var newFileName = $"{xorUrl}/hello.html";
             var (version, newProcessedFiles, newFilesMap) = await session.Files.FilesContainerAddFromRawAsync(TestUtils.GetRandomString(50).ToUtfBytes(), newFileName, false, false, false);
             Assert.AreEqual(1, version);
