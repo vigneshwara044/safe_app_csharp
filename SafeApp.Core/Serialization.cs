1713565567a6b600fbbf0dec9c6d9a5c583bda6b
Ravinder Jangra <ravinderjangra@live.com>
2019-10-10 15:49:56 +0530

    doc/Core: Add XML docs for the SafeApp.Core project

--- SafeApp.Core/Serialization.cs
+++ SafeApp.Core/Serialization.cs
@@ -2,9 +2,11 @@
 
 namespace SafeApp.Core
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public static class Serialization
     {
         public static T Deserialize<T>(string json)
             => JsonConvert.DeserializeObject<T>(json);
     }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
